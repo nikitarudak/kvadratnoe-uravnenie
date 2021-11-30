520f0ec85200a04cd6c04660fdd9bec7ccc7ef0a
Nikita Rudak <nikitarudak07@gmail.com>
2021-11-30 11:38:18 +0200

    -

--- kvadratnoe uravnenie/kvadratnoe_uravnenie.py
+++ kvadratnoe uravnenie/kvadratnoe_uravnenie.py
@@ -1,1 +1,14 @@
+from tkinter import *
 
+window = Tk()
+window.title("Квадратное уравнение")
+window.geometry("600x300")
+
+lbl1 = Label(window, text = "Решение квадратного уравнениея", font = "Arial 20")
+lbl2 = Label(window, text = "Введите значения", font = "Arial 15")
+lbl3 = Label(window, text = "x**2 + ", font = "Arial 12")
+
+
+
+lbl1.pack()
+window.mainloop
